61b1820ad81dc2d8d49debd306888ab352cdd051
Yuya Yabe <yuyabee@gmail.com>
2014-12-10 23:30:02 -0800

    WIP: trying to remove Nothing from list of Maybe String

--- ExtractTypeSignature.hs
+++ ExtractTypeSignature.hs
@@ -4,17 +4,26 @@ import Control.Monad.IO.Class
 
 type Document = [String]
 
+{- it needs to return String instead of Maybe String -}
 extractTypeSignature :: String -> String -> Maybe String
-extractTypeSignature str ptn =
+extractTypeSignature ptn str =
     if take (length ptn) str == ptn 
     then Just str
     else Nothing
 
-extractTypeSignatures :: String -> [String] -> [Maybe String]
-extractTypeSignatures ptn = map $ extractTypeSignatureWithSpecifiedPattern ptn
+extractTypeSignature' :: Stirng -> String -> String
+extractTypeSignature' ptn str =
+    if take 
+
+compactMaybe :: [Maybe String] -> [String]
+compactMaybe (Just x:xs) = [x] ++ compactMaybe xs
+compactMaybe (Nothing:xs) = compactMaybe xs
+
+extractTypeSignatures :: String -> [String] -> [String]
+extractTypeSignatures ptn = compactMaybe $ map $ extractTypeSignatureWithSpecifiedPattern ptn
 
 extractTypeSignatureWithSpecifiedPattern :: String -> String -> Maybe String
-extractTypeSignatureWithSpecifiedPattern ptn str = extractTypeSignature str ptn
+extractTypeSignatureWithSpecifiedPattern = extractTypeSignature
 
 typeSignaturePattern1 :: String
 typeSignaturePattern1 = "typesig"
