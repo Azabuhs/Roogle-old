e60d8498e5c3d84baa109dede18cc7b13be2761a
Yuya Yabe <yuyabee@gmail.com>
2014-12-12 23:27:58 -0800

    WIP: document from Scope

--- ExtractTypeSignature.hs
+++ ExtractTypeSignature.hs
@@ -5,15 +5,16 @@ import Data.Char
 import Text.Regex
 import Data.Map (Map)
 
+type Document = [String]
 type TypeSignature = String
-data Scope = Class String Scope
-            | Module String Scope
+data Scope = Class String [Scope]
+            | Module String [Scope]
             | Method String TypeSignature deriving (Show)
 type Wait = String
 -- head contexts is the current Context
 -- as encounter Wait, tail contexts
 -- Wait is "end" in most cases
-type Context = [Map Scope Wait]
+type Context = Map Scope Wait
 type Contexts = [Context]
 
 extractTypeSignature :: String -> String -> Maybe String
@@ -30,9 +31,20 @@ toDoc str = subRegex (mkRegex "=>")
                   " ::") "") "") " ->") "->"
 
 toDoc' :: Scope -> String
-toDoc' (Class klass scope) = klass ++ toDoc' scope
-toDoc' (Module mdl scope) = mdl ++ toDoc' scope
-toDoc' (Method meth typesig) = "#" ++ meth ++ " :: " ++ typesig
+toDoc' (Class klass scopes) = unlines $ map (\x -> (klass ++ toDoc' x)) scopes
+toDoc' (Module mdl scopes) = unlines $ map (\x -> (mdl ++ toDoc' x)) scopes
+toDoc' (Method meth typesig) = "#" ++ meth ++ " :: " ++ typesig ++ "\n"
+
+{- buildDoc :: String -> Document -}
+{- buildDoc = foldl appendContext [] $ lines -}
+
+{- appendContext :: Context -> String -> Context -}
+{- appendContext = getContext -}
+
+{- getContext :: String -> Context -}
+{- getContext str -}
+  {- | isJust $ matchRegexAll (mkRegex $ "class") str = case matchRegexAll (mkRegex $ "class") str of -}
+    {- Just (_, _, v, _) -> Class v -}
 
 typeSignatureMatch :: String -> String -> Maybe String
 typeSignatureMatch ptn str = case matchRegexAll (mkRegex $ ".*" ++ ptn) str of
--- ExtractTypeSignatureTest.hs
+++ ExtractTypeSignatureTest.hs
@@ -3,16 +3,19 @@ import Test.HUnit
 import System.IO
 import Control.Monad.IO.Class
 
-stringMockCode :: String
-stringMockCode = "  typesig str8int2str: [String, Numeral => String]"
-realMockCode :: [String]
-{- realMockCode = readFile "typesig.rb" -}
-realMockCode = lines "class RubypeTest\ndef str8int2Str(a,b)\n'#{a} b'\nend\ntypesig str8int2str: [String, Numeral => String]\ndef marry(people)\n # Your Ruby code as usual\nend\ntypesig marry: [People -> Any]\nend"
+mockTypesignature :: String
+mockTypesignature = "  typesig str8int2str: [String, Numeral => String]"
+mockCode :: [String]
+mockCode = lines "class RubypeTest\ndef str8int2str(a,b)\n'#{a} b'\nend\ntypesig str8int2str: [String, Numeral => String]\ndef marry(people)\n # Your Ruby code as usual\nend\ntypesig marry: [People -> Any]\nend"
+mockScope :: Scope
+mockScope = Class "RubypeTest" [Method "str8int2str" "String -> Numeral -> String", Method "marry" "People -> Any"]
+
 
 tests = [
-        "typeSignatureMatch" ~: typeSignatureMatch typeSignaturePattern1 stringMockCode ~=? Just "str8int2str: [String, Numeral => String]",
-        "extractTypeSignature" ~: extractTypeSignature typeSignaturePattern1 stringMockCode ~=? Just "str8int2str :: String -> Numeral -> String",
-        "extractTypeSignatures" ~: extractTypeSignatures typeSignaturePattern1 realMockCode ~=? ["str8int2str :: String -> Numeral -> String", "marry :: People -> Any"]
+        "typeSignatureMatch" ~: typeSignatureMatch typeSignaturePattern1 mockTypesignature ~=? Just "str8int2str: [String, Numeral => String]",
+        "extractTypeSignature" ~: extractTypeSignature typeSignaturePattern1 mockTypesignature ~=? Just "str8int2str :: String -> Numeral -> String",
+        "extractTypeSignatures" ~: extractTypeSignatures typeSignaturePattern1 mockCode ~=? ["str8int2str :: String -> Numeral -> String", "marry :: People -> Any"],
+        "ScopeToDoc" ~: lines (toDoc' mockScope) ~=? ["RubypeTest#str8int2str :: String -> Numeral -> String", "RubypeTest#marry :: People -> Any"]
         ]
 
 main = do
