a7d66089dd0bae33c201f66c031c35653ad2601e
Yuya Yabe <yuyabee@gmail.com>
2014-12-13 23:27:47 -0800

    refactor

--- ExtractTypeSignatureTest.hs
+++ ExtractTypeSignatureTest.hs
@@ -3,12 +3,9 @@ import Test.HUnit
 import System.IO
 import Control.Monad.IO.Class
 
-mockTypesignature :: String
-mockTypesignature = "  typesig str8int2str: [String, Numeral => String]"
-mockCode :: [String]
-mockCode = lines "class RubypeTest\ndef str8int2str(a,b)\n'#{a} b'\nend\ntypesig str8int2str: [String, Numeral => String]\ndef marry(people)\n # Your Ruby code as usual\nend\ntypesig marry: [People -> Any]\nend"
-mockScope :: Scope
-mockScope = Class "RubypeTest" [Method "str8int2str" "String -> Numeral -> String", Method "marry" "People -> Any"]
+mockTypesignature = "  typesig str8int2str: [String, Numeral => String]" :: String
+mockCode = lines "class RubypeTest\ndef str8int2str(a,b)\n'#{a} b'\nend\ntypesig str8int2str: [String, Numeral => String]\ndef marry(people)\n # Your Ruby code as usual\nend\ntypesig marry: [People -> Any]\nend" :: [String]
+mockScope = Class "RubypeTest" [Method "str8int2str" "String -> Numeral -> String", Method "marry" "People -> Any"] :: Scope
 
 
 tests = [
