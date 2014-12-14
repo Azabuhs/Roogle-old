abd9bdc6cf6d1f5869b1d15ffe4a823286aeeeb2
Yuya Yabe <yuyabee@gmail.com>
2014-12-13 23:59:20 -0800

    WIP: implementing walk

--- ExtractTypeSignature.hs
+++ ExtractTypeSignature.hs
@@ -3,11 +3,13 @@ import System.Environment
 import Control.Monad.IO.Class
 import Data.Char
 import Text.Regex
+import Text.Regex.Posix
 import Data.Map (Map)
 
 type Document = [String]
 type TypeSignature = String
-data Scope = Class String [Scope]
+data Scope = EmptyScope
+            | Class String [Scope]
             | Module String [Scope]
             | Method String TypeSignature deriving (Show)
 type Wait = String
@@ -35,10 +37,32 @@ scopeToDoc (Class klass scopes) = unlines $ map (\x -> (klass ++ scopeToDoc x)) 
 scopeToDoc (Module mdl scopes) = unlines $ map (\x -> (mdl ++ scopeToDoc x)) scopes
 scopeToDoc (Method meth typesig) = "#" ++ meth ++ " :: " ++ typesig
 
-{- parseCode :: String -> Scope -}
+{- parseCode :: [String] -> Scope -}
+{- parseCode = foldl extendScope EmptyScope -}
 
-{- buildDoc :: String -> Document -}
-{- buildDoc = foldl appendContext [] $ lines -}
+{- extendScope :: Scope -> String -> Scope -}
+{- extendScope EmptyScope code = extractScope code -}
+{- extendScope (Class klass [EmptyScope]) code = Class klass $ extractScope code -}
+{- extendScope (Module mdl [EmptyScope]) code = Module mdl $ extractScope code -}
+
+{- extractScope :: String -> Scope -}
+{- extractScope str -}
+  {- | (str =~ "class" :: Bool) == True = case str =~ "class " :: (String, String, String) of -}
+                                         {- (_, _, v) -> Class v [EmptyScope] -}
+  {- | (str =~ "module" :: Bool) == True = case str =~ "moudle " :: (String, String, String) of -}
+    {- (_, _, v) -> Module v [EmptyScope] -}
+  {- | (str =~ "typesig" :: Bool) == True = case str =~ "typesig " :: (String, String, String) of -}
+    {- (_, _, v) -> Method (methName v) (typesigFromCode v) -}
+
+{- methName :: String -}
+{- methName exp = case matchRegexAll (mkRegex " (.*):") exp of -}
+                 {- Just (_, v, _, _) -> v -}
+                 {- Nothing -> "" -}
+
+{- typesigFromCode :: String -}
+{- typesigFromCode exp = case matchRegexAll (mkRegex " (.*):") exp of -}
+                 {- Just (_, _, v, _) -> v -}
+                 {- Nothing -> "" -}
 
 {- appendContext :: Context -> String -> Context -}
 {- appendContext = getContext -}
@@ -48,8 +72,60 @@ scopeToDoc (Method meth typesig) = "#" ++ meth ++ " :: " ++ typesig
   {- | isJust $ matchRegexAll (mkRegex $ "class") str = case matchRegexAll (mkRegex $ "class") str of -}
     {- Just (_, _, v, _) -> Class v -}
 
+--
+-- EmptyScope
+-- Class className []
+-- Class className [Method name typesig, Method name typesig]
+--
+
+toScopeList :: a -> [a]
+toScopeList = replicate 1
+
+walk :: [String] -> Scope
+walk = foldl accum EmptyScope
+
+accum :: Scope -> String -> Scope
+accum EmptyScope str = mkScope str
+accum (Class x [EmptyScope]) str = Class x $ toScopeList $ accum EmptyScope str
+accum (Module x [EmptyScope]) str = Class x $ toScopeList $ accum EmptyScope str
+accum (Class x s) str = Class x (s ++ $ toScopeList $ accum EmptyScope str)
+accum (Module x s) str = Module x $ s ++ toScopeList $ accum EmptyScope str
+
+encounterClass :: String -> Bool
+encounterClass = True
+encounterModule :: String -> Bool
+encounterModule = True
+encounterMethod :: String -> Bool
+encounterMethod = True
+
+mkScope :: String -> Scope
+mkScope str
+  | encounterClass str = mkClassScope str
+  | encounterModule str = mkModuleScope str
+  | encounterMethod str = mkMethodScope str
+
+mkClassScope :: String -> Scope
+mkClassScope str = Class (getClassName str) [EmptyScope]
+
+mkModuleScope :: String -> Scope
+mkModuleScope str = Module (getModuleName str) [EmptyScope]
+
+mkMethodScope :: String -> Scope
+mkMethodScope str = Method (getMethodName str) (methodTypesignature str)
+
+getClassName :: String -> String
+getClassName = "test"
+getModuleName :: String -> String
+getModuleName = "test"
+getMethodName :: String -> String
+getMethodName = "test"
+methodTypesignature :: String -> String
+methodTypesignature = "testsig"
+--
+--
+
 typeSignatureMatch :: String -> String -> Maybe String
-typeSignatureMatch ptn str = case matchRegexAll (mkRegex $ ".*" ++ ptn) str of
+typeSignatureMatch ptn str = case matchRegexAll (mkRegex ptn) str of
                        Just (_, _, v, _) -> Just v
                        Nothing -> Nothing
 
